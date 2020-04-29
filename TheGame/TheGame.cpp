99c7a3ae490b9068f6ba446c7b3801254edbcf68
Tessa Power <tessa@tessapower.co>
2020-04-30 10:41:02 +1200

    Create placeholder function to print winning message

--- TheGame/TheGame.cpp
+++ TheGame/TheGame.cpp
@@ -11,6 +11,7 @@ void drawCurrentGameState();
 void resetGame();
 void askToPlayAgain();
 void checkForWin();
+void printWinScreen();
 
 const int gridSize = 11;
 char gridPositions[gridSize] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '\0' };
