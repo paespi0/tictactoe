5c5d6ce7cb36ae28cf7a5e679701f009f9a3e470
Tessa Power <tessa@tessapower.co>
2020-05-04 10:25:12 +1200

    Update how win checking function compares array members

--- TheGame/GameWinChecker.cpp
+++ TheGame/GameWinChecker.cpp
@@ -3,16 +3,16 @@
 
 const bool GameWinChecker::checkIfSymbolHasWon(char symbol, char gameGrid[])
 {
-	bool horizontalWin = ((gameGrid[1] == gameGrid[2]) && (gameGrid[2] == gameGrid[3]) ||
-		(gameGrid[4] == gameGrid[5]) && (gameGrid[5] == gameGrid[6]) ||
-		(gameGrid[7] == gameGrid[8]) && (gameGrid[8] == gameGrid[9]));
+	bool horizontalWin = (gameGrid[1] == symbol && gameGrid[2] == symbol && gameGrid[3] == symbol ||
+		gameGrid[4] == symbol && gameGrid[5] == symbol && gameGrid[6] == symbol ||
+		gameGrid[7] == symbol && gameGrid[8] == symbol && gameGrid[9] == symbol);
 
-	bool verticalWin = ((gameGrid[1] == gameGrid[4]) && (gameGrid[4] == gameGrid[7]) ||
-		(gameGrid[2] == gameGrid[5]) && (gameGrid[5] == gameGrid[8]) ||
-		(gameGrid[3] == gameGrid[6]) && (gameGrid[6] == gameGrid[9]));
+	bool verticalWin = (gameGrid[1] == symbol && gameGrid[4] == symbol && gameGrid[7] == symbol ||
+		gameGrid[2] == symbol && gameGrid[5] == symbol && gameGrid[8] == symbol ||
+		gameGrid[3] == symbol && gameGrid[6] == symbol && gameGrid[9] == symbol);
 
-	bool diagonalWin = ((gameGrid[1] == gameGrid[5]) && (gameGrid[5] == gameGrid[9]) ||
-		(gameGrid[3] == gameGrid[5]) && (gameGrid[5] == gameGrid[7]));
+	bool diagonalWin = (gameGrid[1] == symbol && gameGrid[5] == symbol && gameGrid[9] == symbol ||
+		gameGrid[3] == symbol && gameGrid[5] == symbol && gameGrid[7] == symbol);
 
 	return (horizontalWin || verticalWin || diagonalWin);
 }
