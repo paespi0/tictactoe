3191d8f56b26b52bb2d9322e40ee5d0f30fd62b7
Tessa Power <tessa@tessapower.co>
2020-05-04 12:10:14 +1200

    Clean up white space

--- UnitTests/GameWinCheckerTests.cpp
+++ UnitTests/GameWinCheckerTests.cpp
@@ -69,7 +69,7 @@ namespace GameWinCheckerTests
 			// Arrange
 			char playerSymbol = 'X';
 			char gameState[] = {
-				'0', '1', '2', 
+				'0', '1', '2',
 				'3', '4', '5',
 				'6', '7', '8',
 				'\0'};
